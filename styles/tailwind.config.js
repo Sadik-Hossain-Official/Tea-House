bb7355b9a7f122c2038150c58fcb109ab784e677
Sadik-Hossain-Official <sadikhossain230802@gmail.com>
2025-09-03 20:07:10 +0600

    added all files

---
+++ styles/tailwind.config.js
@@ -0,0 +1,5 @@
+// go to website-docs- play CDN-copy link of script tag-paste to the html head
+// Tailwind by default nutralized everything unless you do not define style-values as classes in html tag.(Basically tailwind is a library of CSS where styles are stored as classes)
+// In CSS we uses attribute-property-values but in tailwind we will define the style values as class and it will be styled.
+// Tailwind is a mobile first approch.
+// To add tailwind suggestion in html -install extension and create a tailwind.css file with html and write( @import "tailwindcss") or just create a file tailwind.config.js this will activate the suggestion of tailwind.
